9cf73758acc5c4a49db6c9f1b03dae7d0c966ca2
fuxiaoming <fuxiaoming@kingsoft.com>
2023-07-29 12:01:49 +0800

    add : 应用正常退出时，将发送注销请求

--- time-service/src/main/java/com/ksyun/start/camp/ServiceAppRunner.java
+++ time-service/src/main/java/com/ksyun/start/camp/ServiceAppRunner.java
@@ -15,10 +15,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.event.ContextClosedEvent;
+import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.util.Timer;
 import java.util.TimerTask;
 import java.util.UUID;
@@ -48,10 +51,14 @@ public class ServiceAppRunner implements ApplicationRunner {
     @Value("${server.port}")
     private int servicePort;
 
+    private static final String serviceId = UUID.randomUUID().toString();
+
+
+
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        String serviceId = UUID.randomUUID().toString();
         RegisterDto registerDto = new RegisterDto(serviceName, serviceId, serviceHost, servicePort);
+
         // 向 registry 服务注册当前服务
         registerService(registerDto);
 
@@ -62,16 +69,19 @@ public class ServiceAppRunner implements ApplicationRunner {
 
     // 向注册中心注册当前服务
     private void registerService(RegisterDto registerDto) {
-        try(CloseableHttpClient httpClient = HttpClients.createDefault()) {
+        try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
             // 构建注册服务的URL
             URL url = new URL(registryUrl + "/api/register");
 
             //将RegisterDto对象转换成JSON字符串
             String requestBody = ToJsonUtil.toJsonString(registerDto);
+            log.info("注册服务的请求体为：{}", requestBody);
 
             // 创建POST请求
             HttpPost httpPost = new HttpPost(url.toString());
-            httpPost.setEntity(new StringEntity(requestBody, ContentType.APPLICATION_JSON));
+            if (requestBody != null) {
+                httpPost.setEntity(new StringEntity(requestBody, ContentType.APPLICATION_JSON));
+            }
 
             // 发送请求并获取响应
             HttpResponse response = httpClient.execute(httpPost);
@@ -84,16 +94,18 @@ public class ServiceAppRunner implements ApplicationRunner {
                 log.info("注册失败");
             }
         } catch (IOException e) {
-            e.printStackTrace();
+            log.error("注册失败", e);
         }
     }
 
     // 心跳任务，定期发送心跳请求
     private class HeartbeatTask extends TimerTask {
         private final String serviceId;
+
         public HeartbeatTask(String serviceId) {
             this.serviceId = serviceId;
         }
+
         @Override
         public void run() {
             try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
@@ -106,7 +118,9 @@ public class ServiceAppRunner implements ApplicationRunner {
 
                 // 创建 POST 请求
                 HttpPost httpPost = new HttpPost(url.toString());
-                httpPost.setEntity(new StringEntity(requestBody, ContentType.APPLICATION_JSON));
+                if (requestBody != null) {
+                    httpPost.setEntity(new StringEntity(requestBody, ContentType.APPLICATION_JSON));
+                }
 
                 // 发送请求并获取响应
                 HttpResponse response = httpClient.execute(httpPost);
@@ -119,8 +133,42 @@ public class ServiceAppRunner implements ApplicationRunner {
                     log.info("心跳发送失败");
                 }
             } catch (IOException e) {
-                e.printStackTrace();
+                log.error("心跳发送失败", e);
             }
+        }
+    }
+
+    // 添加应用生命周期回调，用于应用正常退出时发送注销请求
+    @EventListener
+    public void onApplicationClosed(ContextClosedEvent event) {
+        unregisterService();
+    }
+
+    private void unregisterService() {
+        try(CloseableHttpClient httpClient = HttpClients.createDefault()) {
+            // 构建注销服务的URL
+            URL url = new URL(registryUrl + "/api/unregister");
+            RegisterDto registerDto = new RegisterDto(serviceName, serviceId, serviceHost, servicePort);
+            //将RegisterDto对象转换成JSON字符串
+            String requestBody = ToJsonUtil.toJsonString(registerDto);
+
+            // 创建POST请求
+            HttpPost httpPost = new HttpPost(url.toString());
+            if (requestBody != null) {
+                httpPost.setEntity(new StringEntity(requestBody, ContentType.APPLICATION_JSON));
+            }
+            // 发送请求并获取响应
+            HttpResponse response = httpClient.execute(httpPost);
+
+            // 处理响应
+            int statusCode = response.getStatusLine().getStatusCode();
+            if (statusCode == 200) {
+                log.info("注销成功");
+            } else {
+                log.info("注销失败");
+            }
+        } catch (Exception e) {
+            log.error("注销失败", e);
         }
     }
 }
